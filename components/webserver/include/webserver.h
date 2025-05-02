fd932045f5b97a3c78e4155bc8fc9f42ba83e395
Oleksandr Danylchenko <sanekm2010@gmail.com>
2025-05-02 11:45:42 +0300

    Moving webserver from old example, rethinking.

--- components/webserver/include/webserver.h
+++ components/webserver/include/webserver.h
@@ -1,9 +1,35 @@
 #pragma once
 #include <string.h>
 #include "esp_log.h"
+#include "card_driver.h"
+#include "littlefs_driver.h"
 
 #define USERNAME CONFIG_USERNAME
 #define PASSWORD CONFIG_PASSWORD
 #define BASIC_AUTH CONFIG_BASIC_AUTH
 
+
+// Default Webserver root dir - here upload files: SD Card
+#define WEBSERVER_ROOT              SD_MOUNT_POINT
+// Upload INIT server root dir - do not upload files here, but use as initial page for upload
+#define FILESERVER_INIT_ROOT        LFS_MOUNT_POINT
+
+// File server opt
+/* Max length a file path can have on storage */
+#define FILE_PATH_MAX (ESP_VFS_PATH_MAX + CONFIG_SPIFFS_OBJ_NAME_LEN)
+
+/* Max size of an individual file. Make sure this
+ * value is same as that set in upload_script.html */
+#define MAX_FILE_SIZE   (500*1024) // 500 KB
+#define MAX_FILE_SIZE_STR "500KB"
+
+/* Scratch buffer size */
+#define SCRATCH_BUFSIZE  8192
+
+// Server upload files capability
+#define QUERY_KEY_MAX_LEN  (64)
+
+
 void webserver(void);
+
+esp_err_t start_webserver(void);
--- components/webserver/webserver.c
+++ components/webserver/webserver.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include "webserver.h"
 
+
+const char *lfs_index_path = LFS_MOUNT_POINT"/index.html";
+const char *lfs_upload_script_path = LFS_MOUNT_POINT"/upload_script.html";
+const char *lfs_icon_path = LFS_MOUNT_POINT"/favicon.ico";
+
+
 static const char *TAG = "webserver";
 
-void webserver(void)
-{
+
+void webserver(void) {
     printf(" - Init: webserver empty function call!\n\n");
     ESP_LOGI(TAG, "USERNAME: %s", USERNAME);
     ESP_LOGI(TAG, "PASSWORD: %s", PASSWORD);
     ESP_LOGI(TAG, "BASIC_AUTH: %d", BASIC_AUTH);
+    ESP_LOGI(TAG, "SD_MOUNT_POINT new root for webserver: %s", SD_MOUNT_POINT);
+    ESP_LOGI(TAG, "LFS_MOUNT_POINT init (fallback) root for webserver: %s", LFS_MOUNT_POINT);
+    ESP_LOGI(TAG, "MAX_FILE_SIZE_STR (check html JS too): %s", MAX_FILE_SIZE_STR);
+}
+
+
+esp_err_t start_webserver(void) {
+    // 
+    webserver();
+    return ESP_OK;
 }
