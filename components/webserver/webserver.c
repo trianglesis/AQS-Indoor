2f18de9c4371715f41398a6fd87b6675c14c1d50
Oleksandr Danylchenko <sanekm2010@gmail.com>
2025-05-03 15:03:31 +0300

    Finally working reusable read function for html files

--- components/webserver/webserver.c
+++ components/webserver/webserver.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include "webserver.h"
-#include "common.h"
-
 
 bool index_exists = false;
 const char *index_html_path = NULL;
@@ -26,19 +24,20 @@ void webserver(void) {
 Test path to index.html file at SD Card mount path.
 If path is not exist - check index.html at LittleFS mount path.
 Return/update var for a valid path.
+
+It checks if index.html file is present at SD Card first.
+If not - it will try to read SPI Flash (littleFS) index.html file (from PROJ_DIR/flash_data)
+As soon as you upload a new index.html into SD card it will no longer use it from SPI
 */
 void check_indexes_locations(void) {
     struct stat st;  
     const char *file_path = NULL;
-    
-    // snprintf(file_path, sizeof(file_path), "%s/index.html", SD_MOUNT_POINT);
     file_path = SD_MOUNT_POINT"/index.html";
     // Check is SD path exists, first, Serve LFS if not.
     if (stat(file_path, &st) == 0) {
         index_exists = true;
         index_html_path = file_path;
-        ESP_LOGI(TAG, "SD HTML Exist: %d", stat(file_path, &st));
-        ESP_LOGI(TAG, "Root index.html found at SD Card!");
+        ESP_LOGI(TAG, "Web root index.html file exists at SD Card: %d", stat(file_path, &st));
     } else {
         // Serve index html from LittleFS if there is no index at SD Card yet.
         // snprintf(file_path, sizeof(file_path), "%s/index.html", LFS_MOUNT_POINT);
@@ -46,62 +45,54 @@ void check_indexes_locations(void) {
         if (stat(file_path, &st) == 0) {
             index_exists = true;
             index_html_path = file_path;
-            ESP_LOGI(TAG, "LFS HTML Exist: %d", stat(file_path, &st));
-            ESP_LOGW(TAG, "Root index.html is not found at SD Card, use LittleFS!");
+            ESP_LOGI(TAG, "Web root index.html file exists at LittleFS but not at SD Card: %d", stat(file_path, &st));
         } else {
-            ESP_LOGE(TAG, "Index HTML file not exist at LittleFS partition or SD Card! Cannot serve any page!");
+            ESP_LOGE(TAG, "Index HTML file not exist at LittleFS partition or SD Card! Cannot serve any page!\n\tDo not start webserver!");
         }
     }
 }
 
 /*
-Load HTML from SPI OR SD Card
+Load HTML from VALID and exist path.
+Path validation is at the check_indexes_locations()
+
+I've used this read_file as example to understand the proper pointer use.
 - https://github.com/DaveGamble/cJSON/blob/acc76239bee01d8e9c858ae2cab296704e52d916/tests/common.h#L47
 */
-char* load_index_html_file(void) {
-    ESP_LOGI(TAG, "Load HTML index...");
-
+char* load_index_html_file(const char *filepath) {
     FILE *file = NULL;
     char *content = NULL;
     size_t read_chars = 0;
-
+    
     struct stat info;
-    // Test file existence and get its size in bytes
-    if (stat(index_html_path, &info) == 0) {
+    ESP_LOGI(TAG, "Load HTML index...");
+    // Test the file existence and get its size in bytes
+    if (stat(filepath, &info) == 0) {
         ESP_LOGI(
-            TAG, 
-            "Index HTML exists: %s; \n" 
-            "- Last changed: %s; \n"
-            "- File size: %ld bytes ", 
+            TAG, "Index HTML exists: %s; \n\t - Last changed: %s \n\t - File size: %ld bytes", 
             index_html_path, 
             ctime(&info.st_mtime), 
             info.st_size);
     }
-    
     // Read file
-    file = fopen(index_html_path, "r");
+    file = fopen(filepath, "r");
     if (file == NULL) {
         ESP_LOGE(TAG, "Cannot open HTML file for read from path: %s", index_html_path);
-        // return ESP_FAIL;
     }
-
     // Allocate memory for file to read into
     content = (char*)malloc((size_t)info.st_size + sizeof(""));
     if (content == NULL) {
         ESP_LOGE(TAG, "Failed to allocate memory");
-        // return ESP_FAIL;
     }
-
-    // Allocate application buffer used for read/write
+    // Read the file
     read_chars = fread(content, sizeof(char), (size_t)info.st_size, file);
     if ((long)read_chars != info.st_size) {
         free(content);
         content = NULL;
     }
     content[read_chars] = '\0';
-
-    if (file != NULL)
-    {
+    // Close now
+    if (file != NULL) {
         fclose(file);
     }
     return content;
@@ -113,14 +104,12 @@ char* load_index_html_file(void) {
 */
 esp_err_t root_get_handler(httpd_req_t *req) {
     ESP_LOGI(TAG, "Load root handler, start index html page read!");
-    // Load html page from LittleFS or SC Card
-    
-    // size_t *bytes_read;
-    char *content = load_index_html_file();
-    ESP_LOGI(TAG, "Html page read OK!");
+    // Check again, if the is already present html file at SD card.
+    check_indexes_locations();
+    // Load html page from validated path
+    char *content = load_index_html_file(index_html_path);
     httpd_resp_set_type(req, "text/html");
     httpd_resp_send(req, content, HTTPD_RESP_USE_STRLEN);
-    // ESP_LOGI(TAG, "Data sent: %u Bytes", *bytes_read);
     return ESP_OK;
 }
 
